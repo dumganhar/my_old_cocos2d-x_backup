2d52106ab106ae183f6cc7c0a7735f2845803998
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-09 17:52:04 +0800

    issue #4689: Updates nslog

--- external/nslog/CCNSLog.h
+++ external/nslog/CCNSLog.h
@@ -9,8 +9,16 @@
 #ifndef __cocos2d_libs__MyLog__
 #define __cocos2d_libs__MyLog__
 
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+
 extern void CCNSLog(const char* file, int line, const char* function, const char* format, ...);
 
-#define MyLog(format, ...) CCNSLog(__FILE__, __LINE__, __FUNCTION__, format,  ##__VA_ARGS__, nullptr)
+#define CCNSLOG(format, ...) CCNSLog(__FILE__, __LINE__, __FUNCTION__, format,  ##__VA_ARGS__, nullptr)
+
+#else
+
+#define CCNSLOG CCLOG
+
+#endif
 
 #endif /* defined(__cocos2d_libs__MyLog__) */
